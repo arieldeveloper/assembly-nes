aacced08862100b7de4a346465a2271a723d21b2
Ariel Chouminov <arielchoum@gmail.com>
2023-12-30 15:22:43 -0500

    modify gitignore, delete utils.asm

--- utils.asm
+++
@@ -1,6 +0,0 @@
-.segment "CODE"
-
-.proc waitVBlank
-    BIT $2002         ; holds the PPU status flags
-    BPL waitVBlank    ; if negative flag (bit 7) then we are not in vblank, so keep waiting
-.endproc
